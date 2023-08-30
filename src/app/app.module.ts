30c5f95e369b7b230e0cd606d26f3b71173df123
WagnerRR25 <rodrigueswagner09@gmail.com>
2023-08-30 19:06:07 -0300

    feat: Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { NomesComponent } from './nomes/nomes.component';
+
+import { NomesService } from './nomes.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NomesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [HttpClientModule, NomesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
